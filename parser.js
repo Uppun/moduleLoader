664c7e6778027eb0f7cdd8a60bec1b12a1a5e5de
Uppun <TheSuperBro@gmail.com>
2019-11-04 09:14:22 -0500

    i'm trying

--- parser.js
+++ parser.js
@@ -3,5 +3,27 @@ class MessageParser {
     constructor(client) {
         this.client = client;
         this.modules = new Map();
+        this.firehose = [];
+    }
+
+    hook(command, cb) {
+        if (command) {
+            this.modules.set(command, cb);
+        } else {
+            this.firehose.push(cb);
+        }
+    }
+
+    informModules(msg) {
+        const message = msg.content;
+        const command = message.split(' ')[1];
+        const cb = this.modules.get(command);
+        if (cb) {
+            cb(msg);
+        } else {
+            for (const cb of this.firehose) {
+                cb(msg);
+            }
+        }
     }
 }
